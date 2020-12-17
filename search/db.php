f31237f671b677f486fe565ba1acef9f205f6e09
Keshav2136 <Keshav.simple.123@gmail.com>
2020-12-17 13:35:53 +0530

    Files arrangement corrected with web hosting
    Just arranged files and images

---
+++ search/db.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+//Database connection.
+
+$con = MySQLi_connect(
+
+   "localhost", //Server host name.
+
+   "id15520675_search", //Database username.
+
+   "Maestrocg@23", //Database password.
+
+   "id15520675_livesearch" //Database name or anything you would like to call it.
+
+);
+
+
+
+//Check connection
+
+if (MySQLi_connect_errno()) {
+
+   echo "Failed to connect to MySQL: " . MySQLi_connect_error();
+
+}
+
+?>
